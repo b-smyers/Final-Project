192f160130b5083df154ea90bc03b6f4a0bafae8
b-smyers <bs602422@ohio.edu>
2024-12-02 19:36:36 -0500

    PCAdder without ALU

--- PCAdder.sv
+++ PCAdder.sv
@@ -1,5 +1,6 @@
 module PCAdder(PCIn, PCOut);
   input [31:0] PCIn; // 32-bit address
-  input [31:0] PCOut; // input incremented by 4
+  output [31:0] PCOut; // input incremented by 4
 
+  assign PCOut = PCIn + 4;
 endmodule
