cc22ad9722cab60041e16b7f8b30edce46f59b41
Gerald Lee Yurek III <jy605421@ohio.edu>
2024-12-02 20:49:09 -0500

    Added Zero flag for 32 bit ALU

--- ALU32Bit.sv
+++ ALU32Bit.sv
@@ -1,9 +1,11 @@
-module ALU32Bit(a, b, op, cin, result, cout);
+module ALU32Bit(a, b, op, cin, result, cout, zero);
     input [31:0] a, b;          // 32-bit inputs
     input [2:0] op;             // 3-bit operation code
     input cin;                  // Initial carry-in
     output [31:0] result;       // 32-bit result
     output cout;                // Final carry-out
+    output zero;                // zero flag
+
 
     wire c;                     // Carry signal between the two 16-bit ALUs
 
@@ -25,4 +27,5 @@ module ALU32Bit(a, b, op, cin, result, cout);
         .result(result[31:16]),
         .cout(cout)
     );
+   assign zero = (result == 32'b0) ? 1'b1 : 1'b0;
 endmodule
