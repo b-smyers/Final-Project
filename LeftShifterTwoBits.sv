557bba07da4068eeb40ab097e2b618e9b1457452
b-smyers <bs602422@ohio.edu>
2024-12-02 19:41:41 -0500

    Left shift two bits

--- LeftShifterTwoBits.sv
+++ LeftShifterTwoBits.sv
@@ -2,4 +2,5 @@ module LeftShifterTwoBits(ValueIn, ValueOut); // to calc. JumpAddr for J instruc
   input [25:0] ValueIn; // 26-bit input
   output [27:0] ValueOut; // 28-bit output
 
+  assign ValueOut = {ValueIn, 2'b00} // Append 2 bits (basically shift left)
 endmodule
