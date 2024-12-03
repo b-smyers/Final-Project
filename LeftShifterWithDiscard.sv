2b134b61280b1f677d6c1632631d0dd3a61c0083
b-smyers <bs602422@ohio.edu>
2024-12-02 19:43:38 -0500

    Left shift with discard + missing semicolon

--- LeftShifterWithDiscard.sv
+++ LeftShifterWithDiscard.sv
@@ -2,4 +2,5 @@ module LeftShifterWithDiscard(ValueIn, ValueOut); // to calculate BranchAddr for
   input [31:0] ValueIn; // 32-bit input
   output [31:0] ValueOut; // lower 30 bits of input concatenated with two zeroes
 
+  assign ValueOut = {ValueIn[29:0], 2'b00}; // Remove 2 leading bits and append 2 0's
 endmodule
