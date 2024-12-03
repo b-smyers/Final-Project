af02a61f6c92a52632280deb1df8f0074db17987
b-smyers <bs602422@ohio.edu>
2024-12-02 20:07:08 -0500

    Instruction Memory

--- InstructionMemory.sv
+++ InstructionMemory.sv
@@ -3,4 +3,15 @@ module InstructionMemory(Address, Clk, Instruction);
   input Clk; // very important!
   output [31:0] Instruction; // Value read from memory location Address
 
+  reg [31:0] memory [15:0]; // 16 words of memory
+
+  // Load instructions here
+  // initial begin
+    // memory[0] = 32'h2468ACEF;
+    // etc.
+  // end
+
+  always @(posedge Clk) begin
+    Instruction <= memory[Address];
+  end
 endmodule
