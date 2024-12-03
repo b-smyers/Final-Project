9ae6e032b6a74b1d06c9c5537362cf2bed837fac
b-smyers <bs602422@ohio.edu>
2024-12-02 19:38:55 -0500

    Program counter

--- ProgramCounter.sv
+++ ProgramCounter.sv
@@ -3,4 +3,7 @@ module ProgramCounter(PCIn, Clk, PCOut);
   input Clk; // PC is updated on each posedge of Clk. Period may be longer than regular clock!
   input [31:0] PCOut; // 32-bit address
 
+  always @(posedge Clk) begin
+    PCOut <= PCIn;
+  end
 endmodule
