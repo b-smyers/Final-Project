8e79ec037eb25c97b4d3533cc33aac4de63cf460
b-smyers <bs602422@ohio.edu>
2024-12-04 16:24:29 -0500

    Added 'reg' declaration

--- InstructionMemory.sv
+++ InstructionMemory.sv
@@ -1,7 +1,7 @@
 module InstructionMemory(Address, Clk, Instruction);
   input [31:0] Address; // 32-bit address to memory.
   input Clk; // very important!
-  output [31:0] Instruction; // Value read from memory location Address
+  output reg [31:0] Instruction; // Value read from memory location Address
 
   reg [31:0] memory [15:0]; // 16 words of memory
 
